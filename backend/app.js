6733c749a3276f088c49eda2f893cfd6585ff09d
Rexx Alfaro <rexxalfaro+github@gmail.com>
2026-01-17 22:14:37 -0800

    Replace session from passport to session from express-session and import passport

--- backend/app.js
+++ backend/app.js
@@ -3,10 +3,13 @@ require("dotenv").config({
 });
 
 const express = require("express");
+const session = require("express-session");
 const passport = require("passport");
-const { session } = require("passport");
-const app = express();
 const pgSession = require("connect-pg-simple")(session);
+
+require("./lib/passport");
+
+const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
@@ -26,12 +29,13 @@ app.use(
     cookie: {
       httpOnly: true,
       maxAge: 1000 * 60 * 60,
-      secure: process.env.NODE_ENV === "production",
+      secure: process.env.NODE_ENV === "production" ? "true" : "false",
       sameSite: process.env.NODE_ENV === "production" ? "none" : "lax",
     },
   })
 );
 
+app.use(passport.initialize());
 app.use(passport.session());
 
 app.listen(3000, (error) => {
