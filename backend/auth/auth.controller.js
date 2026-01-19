3e4992098e9ee2e65e1cdecb1db11d1e9aadc815
Rexx Alfaro <rexxalfaro+github@gmail.com>
2026-01-18 18:49:13 -0800

    Create controller for login, logout and signup

---
+++ backend/auth/auth.controller.js
@@ -0,0 +1,40 @@
+const authService = require("./auth.service");
+const passport = require("../lib/passport");
+
+exports.login = (req, res, next) => {
+  passport.authenticate("local", (err, user, info) => {
+    if (err) return next(err);
+
+    if (!user) {
+      const error = new Error("INVALID_CREDENTIALS");
+      error.status = 401;
+      return next(error);
+    }
+
+    req.logIn(user, (err) => {
+      if (err) return next(err);
+      return res.json({ message: "Logged in successfully" });
+    });
+  })(req, res, next);
+};
+
+exports.signup = async (req, res, next) => {
+  try {
+    const user = await authService.signup(req.body);
+
+    res.status(201).json(user);
+  } catch (err) {
+    next(err);
+  }
+};
+
+exports.logout = (req, res, next) => {
+  req.logout((err) => {
+    if (err) return next(err);
+
+    req.session.destroy(() => {
+      res.clearCookie("connect.sid");
+      res.json({ message: "Logged out" });
+    });
+  });
+};
