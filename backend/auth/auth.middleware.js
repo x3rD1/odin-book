2aad0b45004e404bcf3793f92c4c6cb7e4952a3c
Rexx Alfaro <rexxalfaro+github@gmail.com>
2026-01-18 18:45:12 -0800

    Change json to return message instead of error

--- backend/auth/auth.middleware.js
+++ backend/auth/auth.middleware.js
@@ -1,7 +1,7 @@
 exports.requireAuth = (req, res, next) => {
   // Checks if the user is not logged in
   if (!req.isAuthenticated()) {
-    return res.status(401).json({ error: "Unauthorized" });
+    return res.status(401).json({ message: "Unauthorized" });
   }
 
   next();
