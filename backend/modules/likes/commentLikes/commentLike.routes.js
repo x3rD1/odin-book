e2fd8d54d29c2456ff5f71ff9a64e21bd675f660
Rexx Alfaro <rexxalfaro+github@gmail.com>
2026-01-20 16:27:37 -0800

    Remove requireAuth from commentLikes and postLikes routes

--- backend/modules/likes/commentLikes/commentLike.routes.js
+++ backend/modules/likes/commentLikes/commentLike.routes.js
@@ -1,10 +1,9 @@
 const { Router } = require("express");
 const router = Router({ mergeParams: true });
 const commentLikeController = require("./commentLike.controller");
-const { requireAuth } = require("../../../auth/auth.middleware");
 
 router.get("/", commentLikeController.getAllLikes);
-router.post("/", requireAuth, commentLikeController.createLike);
-router.delete("/", requireAuth, commentLikeController.deleteLike);
+router.post("/", commentLikeController.createLike);
+router.delete("/", commentLikeController.deleteLike);
 
 module.exports = router;
--- backend/modules/likes/postLikes/postLike.routes.js
+++ backend/modules/likes/postLikes/postLike.routes.js
@@ -1,10 +1,9 @@
 const { Router } = require("express");
 const router = Router({ mergeParams: true });
 const postLikeController = require("./postLike.controller");
-const { requireAuth } = require("../../../auth/auth.middleware");
 
 router.get("/", postLikeController.getAllLikes);
-router.post("/", requireAuth, postLikeController.createLike);
-router.delete("/", requireAuth, postLikeController.deleteLike);
+router.post("/", postLikeController.createLike);
+router.delete("/", postLikeController.deleteLike);
 
 module.exports = router;
