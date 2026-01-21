27b7e094220b117b5f6b05eb0b80f9ce7a12981b
Rexx Alfaro <rexxalfaro+github@gmail.com>
2026-01-21 01:10:41 -0800

    Add /me route to get user info

--- backend/auth/auth.routes.js
+++ backend/auth/auth.routes.js
@@ -4,6 +4,7 @@ const authController = require("./auth.controller");
 const validate = require("../middlewares/validators/validate");
 const { signupSchema } = require("../middlewares/validators/signupSchema");
 
+router.get("/me", authController.getMe);
 router.post("/login", authController.login);
 router.post("/signup", validate(signupSchema), authController.signup);
 router.post("/logout", authController.logout);
