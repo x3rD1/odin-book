afab124aa0cf8f7c9826052f7ece053d2ae02c0f
Rexx Alfaro <rexxalfaro+github@gmail.com>
2026-01-21 15:14:07 -0800

    Create Login form

--- frontend/src/features/auth/Login.jsx
+++ frontend/src/features/auth/Login.jsx
@@ -1,5 +1,60 @@
+import { useContext, useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { api } from "../../api/client";
+import AuthContext from "./AuthContext";
+
 function Login() {
-  return <div>Login</div>;
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState("");
+  const { refreshUser, loading } = useContext(AuthContext);
+  const navigate = useNavigate();
+
+  const handleLogin = async (e) => {
+    e.preventDefault();
+    setError("");
+
+    try {
+      await api.post("/auth/login", { email, password });
+      await refreshUser();
+      navigate("/", { replace: true });
+    } catch (err) {
+      console.error("Login failed", err);
+      setError(err.response?.data?.message || "Login failed");
+      setPassword("");
+    }
+  };
+
+  return (
+    <div>
+      <form onSubmit={handleLogin}>
+        {error && <p style={{ color: "red" }}>{error}</p>}
+        <div>
+          <label>Email</label>
+          <input
+            type="email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            required
+          />
+        </div>
+        <div>
+          <label>Password</label>
+          <input
+            type="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            required
+          />
+        </div>
+        <div>
+          <button type="submit" disabled={loading}>
+            {loading ? "Logging in..." : "Login"}
+          </button>
+        </div>
+      </form>
+    </div>
+  );
 }
 
 export default Login;
