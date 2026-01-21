8536dc4a81bc634054be50026277bb4b4bad78f5
Rexx Alfaro <rexxalfaro+github@gmail.com>
2026-01-20 17:24:02 -0800

    Mount routers and add errorHandler middleware

--- backend/app.js
+++ backend/app.js
@@ -6,6 +6,7 @@ const express = require("express");
 const session = require("express-session");
 const passport = require("passport");
 const pgSession = require("connect-pg-simple")(session);
+const errorHandler = require("./middlewares/errorHandler");
 
 require("./lib/passport");
 
@@ -29,14 +30,26 @@ app.use(
     cookie: {
       httpOnly: true,
       maxAge: 1000 * 60 * 60,
-      secure: process.env.NODE_ENV === "production" ? "true" : "false",
+      secure: process.env.NODE_ENV === "production",
       sameSite: process.env.NODE_ENV === "production" ? "none" : "lax",
     },
-  })
+  }),
 );
 
 app.use(passport.initialize());
 app.use(passport.session());
+
+// Routers
+const { requireAuth } = require("./auth/auth.middleware");
+const authRoutes = require("./auth/auth.routes");
+const postRoutes = require("./modules/posts/post.routes");
+const userRoutes = require("./modules/users/user.routes");
+
+app.use("/auth", authRoutes);
+app.use("/posts", requireAuth, postRoutes);
+app.use("/users", requireAuth, userRoutes);
+
+app.use(errorHandler);
 
 app.listen(3000, (error) => {
   if (error) throw error;
