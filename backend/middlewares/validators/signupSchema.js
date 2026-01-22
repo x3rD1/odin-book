7a6c06c88a43626d0b1dfd99b40345050b2ff88a
Rexx Alfaro <rexxalfaro+github@gmail.com>
2026-01-21 19:39:20 -0800

    Add proper messages per error

--- backend/middlewares/validators/signupSchema.js
+++ backend/middlewares/validators/signupSchema.js
@@ -19,10 +19,19 @@ exports.signupSchema = Joi.object({
     .min(6)
     .max(72)
     .pattern(/^(?=.*\d).+$/)
-    .required(),
+    .required()
+    .messages({
+      "string.min": "Password must be at least 6 characters long",
+      "string.max": "Password must be at most 72 characters long",
+      "string.pattern.base": "Password must contain at least one number",
+      "any.required": "Password is required",
+    }),
 
   confirmPassword: Joi.string()
     .label("Confirm password")
     .valid(Joi.ref("password"))
-    .required(),
+    .required()
+    .messages({
+      "any.only": "Passwords do not match",
+    }),
 });
