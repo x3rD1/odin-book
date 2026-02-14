7b4956607c09b87ceb2ddf0ff887896f1103fd48
Rexx Alfaro <rexxalfaro+github@gmail.com>
2026-02-13 20:55:44 -0800

    Add notif fetching on mount

--- frontend/src/features/posts/Feed.jsx
+++ frontend/src/features/posts/Feed.jsx
@@ -1,5 +1,30 @@
+import { useContext, useEffect } from "react";
+import NavIcons from "../../components/NavIcons";
+import Title from "../../components/Title";
+import styles from "./Feed.module.css";
+import PostCreate from "./PostCreate";
+import PostList from "./PostList";
+import NotifContext from "../../contexts/NotifContext";
+
 function Feed() {
-  return <div>Feed</div>;
+  const { notifs, fetchNotifs } = useContext(NotifContext);
+
+  useEffect(() => {
+    if (notifs.length === 0) {
+      fetchNotifs();
+    }
+  }, [fetchNotifs, notifs.length]);
+
+  return (
+    <div className={styles.feedContainer}>
+      <Title />
+      <NavIcons />
+      <main className={styles.mainContent}>
+        <PostCreate />
+        <PostList />
+      </main>
+    </div>
+  );
 }
 
 export default Feed;
