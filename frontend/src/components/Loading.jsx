33e1cd8ab1410a02e83c5fb786887cafed3eea76
Rexx Alfaro <rexxalfaro+github@gmail.com>
2026-02-14 01:16:44 -0800

    Update loading component

--- frontend/src/components/Loading.jsx
+++ frontend/src/components/Loading.jsx
@@ -1,5 +1,14 @@
+import styles from "./Loading.module.css";
+import DualBall from "../assets/icons/Dual-Ball@1x-1.2s-200px-200px.svg?react";
+
 function Loading() {
-  return <div>Loading...</div>;
+  return (
+    <div className={styles.overlay}>
+      <div className={styles.spinner}>
+        <DualBall />
+      </div>
+    </div>
+  );
 }
 
 export default Loading;
