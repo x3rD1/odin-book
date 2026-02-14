438b5340fe728eb78df2219ea0a7021faf21e3da
Rexx Alfaro <rexxalfaro+github@gmail.com>
2026-02-13 20:53:42 -0800

    Import and use Providers

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,13 +1,25 @@
 import { AuthProvider } from "./features/auth/AuthProvider";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import routes from "./routes/routes";
+import { NotifProvider } from "./contexts/NotifProvider";
+import { SocketProvider } from "./contexts/SocketProvider";
+import { PostsProvider } from "./contexts/PostsProvider";
+import { CommentProvider } from "./contexts/CommentProvider";
 
 const router = createBrowserRouter(routes);
 
 function App() {
   return (
     <AuthProvider>
-      <RouterProvider router={router} />
+      <SocketProvider>
+        <PostsProvider>
+          <CommentProvider>
+            <NotifProvider>
+              <RouterProvider router={router} />
+            </NotifProvider>
+          </CommentProvider>
+        </PostsProvider>
+      </SocketProvider>
     </AuthProvider>
   );
 }
