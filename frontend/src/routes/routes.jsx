ed1a096d1f708acfc6cc21079df6de7862f4cec7
Rexx Alfaro <rexxalfaro+github@gmail.com>
2026-02-13 20:58:13 -0800

    Add new routes

--- frontend/src/routes/routes.jsx
+++ frontend/src/routes/routes.jsx
@@ -1,7 +1,11 @@
 import Login from "../features/auth/Login";
 import Signup from "../features/auth/Signup";
 import Feed from "../features/posts/Feed";
+import PostDetail from "../features/posts/PostDetail";
+import Profile from "../features/users/Profile";
 import ProtectedRoute from "../routes/ProtectedRoute";
+import Users from "../features/users/Users";
+import Settings from "../components/Settings";
 
 const routes = [
   {
@@ -9,6 +13,38 @@ const routes = [
     element: (
       <ProtectedRoute>
         <Feed />
+      </ProtectedRoute>
+    ),
+  },
+  {
+    path: "/posts/:id",
+    element: (
+      <ProtectedRoute>
+        <PostDetail />
+      </ProtectedRoute>
+    ),
+  },
+  {
+    path: "users",
+    element: (
+      <ProtectedRoute>
+        <Users />
+      </ProtectedRoute>
+    ),
+  },
+  {
+    path: "settings",
+    element: (
+      <ProtectedRoute>
+        <Settings />
+      </ProtectedRoute>
+    ),
+  },
+  {
+    path: "/users/:userId",
+    element: (
+      <ProtectedRoute>
+        <Profile />
       </ProtectedRoute>
     ),
   },
