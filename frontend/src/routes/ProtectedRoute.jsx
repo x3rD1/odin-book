6ca450bfd5182e154764dfac02eaca45afaa886c
Rexx Alfaro <rexxalfaro+github@gmail.com>
2026-02-13 20:57:51 -0800

    Change the return to null when loading is true

--- frontend/src/routes/ProtectedRoute.jsx
+++ frontend/src/routes/ProtectedRoute.jsx
@@ -1,12 +1,11 @@
 import { Navigate } from "react-router-dom";
 import { useContext } from "react";
 import AuthContext from "../features/auth/AuthContext";
-import Loading from "../components/Loading";
 
 function ProtectedRoute({ children }) {
   const { user, loading } = useContext(AuthContext);
 
-  if (loading) return <Loading />;
+  if (loading) return null;
 
   if (!user) return <Navigate to="/login" replace />;
 
